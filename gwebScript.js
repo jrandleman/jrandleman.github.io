94db937f87e8c81ec3baf08c9d5334b20764a53e
jrandleman <jrandleman@scu.edu>
2018-12-17 01:06:31 -0800

    init sud numbers 2

--- gwebScript.js
+++ gwebScript.js
@@ -259,15 +259,15 @@ function sudIcon() {
 }
 function sudIcon2() {
     for(let sdkup = 0; sdkup < 9999; sdkup++) {
-        var sud2Time = 1000 + (sdkup*13500);
-        var sud3Time = 2500 + (sdkup*13500);
-        var sud4Time = 4000 + (sdkup*13500);
-        var sud5Time = 5500 + (sdkup*13500);
-        var sud6Time = 7000 + (sdkup*13500);
-        var sud7Time = 8500 + (sdkup*13500);
-        var sud8Time = 10000 + (sdkup*13500);
-        var sud9Time = 11500 + (sdkup*13500);
-        var sud1Time = 13000 + (sdkup*13500);
+        var sud2Time = 1067 + (sdkup*13500); // 1000
+        var sud3Time = 2567 + (sdkup*13500); // 2500
+        var sud4Time = 4067 + (sdkup*13500); // 4000
+        var sud5Time = 5567 + (sdkup*13500); // 5500
+        var sud6Time = 7067 + (sdkup*13500); // 7000
+        var sud7Time = 8567 + (sdkup*13500); // 8500
+        var sud8Time = 10067 + (sdkup*13500); // 10000
+        var sud9Time = 11567 + (sdkup*13500); // 11500
+        var sud1Time = 13067 + (sdkup*13500); // 13000
         setTimeout(sud2, sud2Time);
         setTimeout(sud3, sud3Time);
         setTimeout(sud4, sud4Time);
