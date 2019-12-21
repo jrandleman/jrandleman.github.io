bb1d684d7b187be3465629b2c161e9aff8e0cfc2
Jordan Randleman <jrandleman@scu.edu>
2019-12-21 10:59:03 -0800

    linked to kmapCreate from dim Main

--- gwebHFTScript.js
+++ gwebHFTScript.js
@@ -12,31 +12,31 @@ function borderQ() { document.getElementById("spinning3DText3").style = "color:#
 /******************************************************************************/
 
 function cogAnim(flag) {
-	if(flag == 1) {
-		document.getElementById("lnId").classList.add("fa-spin");
-	} else {
-		document.getElementById("lnId").classList.remove("fa-spin");
-	}
+    if(flag == 1) {
+        document.getElementById("lnId").classList.add("fa-spin");
+    } else {
+        document.getElementById("lnId").classList.remove("fa-spin");
+    }
 }
 
 function on(x) {
-	if(x == 1) {
-		document.getElementById("con2").innerHTML = "&#x27AA;";	
-	} else if(x == 1.5) {
-		document.getElementById("con2").innerHTML = "&#x27AB;";
-	} else if(x == 2) {
-		document.getElementById("con3").innerHTML = "&#x2751;";	
-	} else if(x == 2.5) {
-		document.getElementById("con3").innerHTML = "&#x2752;";
-	} else if(x == 3) {
-		document.getElementById("con4").innerHTML = "&#x2738;";	
-	} else if(x == 3.5) {
-		document.getElementById("con4").innerHTML = "&#x2729;";
-	} else if(x == 4) {
-		document.getElementById("con5").innerHTML = "&#x2468;";	
-	} else if(x == 4.5) {
-		document.getElementById("con5").innerHTML = "&#x24C8;";
-	}
+    if(x == 1) {
+        document.getElementById("con2").innerHTML = "&#x27AA;"; 
+    } else if(x == 1.5) {
+        document.getElementById("con2").innerHTML = "&#x27AB;";
+    } else if(x == 2) {
+        document.getElementById("con3").innerHTML = "&#x2751;"; 
+    } else if(x == 2.5) {
+        document.getElementById("con3").innerHTML = "&#x2752;";
+    } else if(x == 3) {
+        document.getElementById("con4").innerHTML = "&#x2738;"; 
+    } else if(x == 3.5) {
+        document.getElementById("con4").innerHTML = "&#x2729;";
+    } else if(x == 4) {
+        document.getElementById("con5").innerHTML = "&#x2468;"; 
+    } else if(x == 4.5) {
+        document.getElementById("con5").innerHTML = "&#x24C8;";
+    }
 }
 
 /******************************************************************************/ 
@@ -76,8 +76,11 @@ const id1FilledArrD = randTitle(id1LenD).split('').splice(0,id1LenD);
 var titleTxtD = "Spatial Dimensions!", titleLenD = (titleTxtD.length);
 const titleFilledArrD = randTitle(id1LenD).split('').splice(0,titleLenD);
 
-var numTextD = "SHOW ME THE NUMBERS", numLenD = numTextD.length;
+var numTextD = "THE NUMBERS", numLenD = numTextD.length;
 const numFilledArrD = randTitle(id1LenD).split('').splice(0,numLenD);
+
+var kTextD = "KMAP CREATION", kLenD = kTextD.length;
+const kFilledArrD = randTitle(id1LenD).split('').splice(0,kLenD);
 
 var subt1TextD = " DETERMINE ", subt1LenD = subt1TextD.length;
 const subt1FilledArrD = randTitle(id1LenD).split('').splice(0,subt1LenD);
@@ -144,20 +147,35 @@ const numFilledArrKmap = randTitle(titleLenKmap).split('').splice(0,numLenKmap);
 var dimTextKmap = "N DIMENSIONS", dimLenKmap = dimTextKmap.length;
 const dimFilledArrKmap = randTitle(titleLenKmap).split('').splice(0,dimLenKmap);
 
+var sopTextKmap = "SOP", sopLenKmap = sopTextKmap.length;
+const sopFilledArrKmap = randTitle(titleLenKmap).split('').splice(0,sopLenKmap);
+
+var posTextKmap = "POS", posLenKmap = posTextKmap.length;
+const posFilledArrKmap = randTitle(titleLenKmap).split('').splice(0,posLenKmap);
+
+var fcnEditTextKmap = "Fcn Editor", fcnEditLenKmap = fcnEditTextKmap.length;
+const fcnEditFilledArrKmap = randTitle(titleLenKmap).split('').splice(0,fcnEditLenKmap);
+
+var kmapTextKmap = "Kmap", kmapLenKmap = kmapTextKmap.length;
+const kmapFilledArrKmap = randTitle(titleLenKmap).split('').splice(0,kmapLenKmap);
+
+var ttableTextKmap = "Ttable", ttableLenKmap = ttableTextKmap.length;
+const ttableFilledArrKmap = randTitle(titleLenKmap).split('').splice(0,ttableLenKmap);
+
 /******************************************************************************/ 
 // TYPEWRITER FUNCTIONS
 /******************************************************************************/
 
 function randTitle(len) {
-	while (possibleTitleInts.length < len) {
-		var ranNum =  Math.floor(Math.random() * (len));
-		if (possibleTitleInts.indexOf(ranNum) == -1) possibleTitleInts.push(ranNum);
-	}
-	for (let i = 0; i < possibleTitleInts.length; i++) {
-		var num = possibleTitleInts[i];
-		titleArr.push(possible[num]);
-	}
-	return titleArr.join('');
+    while (possibleTitleInts.length < len) {
+        var ranNum =  Math.floor(Math.random() * (len));
+        if (possibleTitleInts.indexOf(ranNum) == -1) possibleTitleInts.push(ranNum);
+    }
+    for (let i = 0; i < possibleTitleInts.length; i++) {
+        var num = possibleTitleInts[i];
+        titleArr.push(possible[num]);
+    }
+    return titleArr.join('');
 }
 
 
@@ -174,48 +192,50 @@ function typeWriter() {
 
 
 function typeWriterNcon() {
-	if (ch < titleTxtN.length) {
-		var letter = titleTxtN.charAt(ch);
-		var nnpLetter = nnpTextN.charAt(ch);
-		var prcletter = prcTextN.charAt(ch);
-		var subt1letter = subt1TextN.charAt(ch);
-		var subt2letter = subt2TextN.charAt(ch);
-		var id1letter = id1TextN.charAt(ch);
-		var id2letter = id2TextN.charAt(ch);
+    if (ch < titleTxtN.length) {
+        var letter = titleTxtN.charAt(ch);
+        var nnpLetter = nnpTextN.charAt(ch);
+        var prcletter = prcTextN.charAt(ch);
+        var subt1letter = subt1TextN.charAt(ch);
+        var subt2letter = subt2TextN.charAt(ch);
+        var id1letter = id1TextN.charAt(ch);
+        var id2letter = id2TextN.charAt(ch);
 
-		titleFilledArrN.splice(ch,1,letter);
-		nnpFilledArrN.splice(ch,1,nnpLetter);
-		prcFilledArrN.splice(ch,1,prcletter);
-		subt1FilledArrN.splice(ch,1,subt1letter);
-		subt2FilledArrN.splice(ch,1,subt2letter);
-		id1FilledArrN.splice(ch,1,id1letter);
-		id2FilledArrN.splice(ch,1,id2letter);
+        titleFilledArrN.splice(ch,1,letter);
+        nnpFilledArrN.splice(ch,1,nnpLetter);
+        prcFilledArrN.splice(ch,1,prcletter);
+        subt1FilledArrN.splice(ch,1,subt1letter);
+        subt2FilledArrN.splice(ch,1,subt2letter);
+        id1FilledArrN.splice(ch,1,id1letter);
+        id2FilledArrN.splice(ch,1,id2letter);
 
-		document.getElementById("webTitleID").innerHTML = titleFilledArrN.join('');
-		document.getElementById("nnpId").innerHTML = nnpFilledArrN.join('');
-		document.getElementById("prcId").innerHTML = prcFilledArrN.join('');
-		document.getElementById("subt1Id").innerHTML = subt1FilledArrN.join('');
-		document.getElementById("subt2Id").innerHTML = subt2FilledArrN.join('');
-		document.getElementById("id1").innerHTML = id1FilledArrN.join('');
-		document.getElementById("id2").innerHTML = id2FilledArrN.join('');
-		ch++;
-		setTimeout(typeWriterNcon, speed);
-	}
+        document.getElementById("webTitleID").innerHTML = titleFilledArrN.join('');
+        document.getElementById("nnpId").innerHTML = nnpFilledArrN.join('');
+        document.getElementById("prcId").innerHTML = prcFilledArrN.join('');
+        document.getElementById("subt1Id").innerHTML = subt1FilledArrN.join('');
+        document.getElementById("subt2Id").innerHTML = subt2FilledArrN.join('');
+        document.getElementById("id1").innerHTML = id1FilledArrN.join('');
+        document.getElementById("id2").innerHTML = id2FilledArrN.join('');
+        ch++;
+        setTimeout(typeWriterNcon, speed);
+    }
 }
 
 
 function typeWriterDim() {
-	if (ch < id1TextD.length) {
+    if (ch < id1TextD.length) {
         var letter = titleTxtD.charAt(ch);
-		var numletter = numTextD.charAt(ch);
-		var subt1letter = subt1TextD.charAt(ch);
-		var subt2letter = subt2TextD.charAt(ch);
-		var ssub1letter = ssub1TextD.charAt(ch);
-		var ssub2letter = ssub2TextD.charAt(ch);
-		var id1letter = id1TextD.charAt(ch);
+        var numletter = numTextD.charAt(ch);
+        var kletter = kTextD.charAt(ch);
+        var subt1letter = subt1TextD.charAt(ch);
+        var subt2letter = subt2TextD.charAt(ch);
+        var ssub1letter = ssub1TextD.charAt(ch);
+        var ssub2letter = ssub2TextD.charAt(ch);
+        var id1letter = id1TextD.charAt(ch);
 
         titleFilledArrD.splice(ch,1,letter);
         numFilledArrD.splice(ch,1,numletter);
+        kFilledArrD.splice(ch,1,kletter);
         subt1FilledArrD.splice(ch,1,subt1letter);
         subt2FilledArrD.splice(ch,1,subt2letter);
         ssub1FilledArrD.splice(ch,1,ssub1letter);
@@ -224,6 +244,7 @@ function typeWriterDim() {
 
         document.getElementById("webTitleID").innerHTML = titleFilledArrD.join('');
         document.getElementById("numId").innerHTML = numFilledArrD.join('');
+        document.getElementById("kmapId").innerHTML = kFilledArrD.join('');
         document.getElementById("subt1Id").innerHTML = subt1FilledArrD.join('');
         document.getElementById("subt2Id").innerHTML = subt2FilledArrD.join('');
         document.getElementById("ssub1").innerHTML = ssub1FilledArrD.join('');
@@ -238,14 +259,14 @@ function typeWriterDim() {
 function typeWriterPer() {
     if (ch < id1TextP.length) {
         var letter = titleTxtP.charAt(ch);
-		var numletter = numTextP.charAt(ch);
-		var subt1letter = subt1TextP.charAt(ch);
-		var subt2letter = subt2TextP.charAt(ch);
-		var ssub1letter = ssub1TextP.charAt(ch);
-		var ssub2letter = ssub2TextP.charAt(ch);
-		var ssub3letter = ssub3TextP.charAt(ch);
-		var ssub4letter = ssub4TextP.charAt(ch);
-		var id1letter = id1TextP.charAt(ch);
+        var numletter = numTextP.charAt(ch);
+        var subt1letter = subt1TextP.charAt(ch);
+        var subt2letter = subt2TextP.charAt(ch);
+        var ssub1letter = ssub1TextP.charAt(ch);
+        var ssub2letter = ssub2TextP.charAt(ch);
+        var ssub3letter = ssub3TextP.charAt(ch);
+        var ssub4letter = ssub4TextP.charAt(ch);
+        var id1letter = id1TextP.charAt(ch);
 
         titleFilledArrP.splice(ch,1,letter);
         numFilledArrP.splice(ch,1,numletter);
@@ -275,12 +296,12 @@ function typeWriterPer() {
 function typeWriterStar() {
     if (ch < titleTxtS.length) {
         var letter = titleTxtS.charAt(ch);
-		var numletter = numTextS.charAt(ch);
-		var tesletter = tesTextS.charAt(ch);
-		var subt1letter = subt1TextS.charAt(ch);
-		var subt2letter = subt2TextS.charAt(ch);
-		var ssub1letter = ssub1TextS.charAt(ch);
-		var id1letter = id1TextS.charAt(ch);
+        var numletter = numTextS.charAt(ch);
+        var tesletter = tesTextS.charAt(ch);
+        var subt1letter = subt1TextS.charAt(ch);
+        var subt2letter = subt2TextS.charAt(ch);
+        var ssub1letter = ssub1TextS.charAt(ch);
+        var id1letter = id1TextS.charAt(ch);
 
         titleFilledArrS.splice(ch,1,letter);
         numFilledArrS.splice(ch,1,numletter);
@@ -304,7 +325,7 @@ function typeWriterStar() {
 
 
 function typeWriterNote() {
-	if (ch < titleLenNote) {
+    if (ch < titleLenNote) {
         var letter = titleTxtNote.charAt(ch);
         titleFilledArrNote.splice(ch,1,letter);
         document.getElementById("webTitleID").innerHTML = titleFilledArrNote.join('');
@@ -320,13 +341,31 @@ function typeWriterKmap() {
     var numLetter = numTextKmap.charAt(ch);
     var dimletter = dimTextKmap.charAt(ch);
 
+    var sopletter = sopTextKmap.charAt(ch);
+    var posletter = posTextKmap.charAt(ch);
+    var fcnEditletter = fcnEditTextKmap.charAt(ch);
+    var kmapletter = kmapTextKmap.charAt(ch);
+    var ttableletter = ttableTextKmap.charAt(ch);
+
     titleFilledArrKmap.splice(ch,1,letter);
     numFilledArrKmap.splice(ch,1,numLetter);
     dimFilledArrKmap.splice(ch,1,dimletter);
 
+    sopFilledArrKmap.splice(ch,1,sopletter);
+    posFilledArrKmap.splice(ch,1,posletter);
+    fcnEditFilledArrKmap.splice(ch,1,fcnEditletter);
+    kmapFilledArrKmap.splice(ch,1,kmapletter);
+    ttableFilledArrKmap.splice(ch,1,ttableletter);
+
     document.getElementById("webTitleID").innerHTML = titleFilledArrKmap.join('');
     document.getElementById("numId").innerHTML = numFilledArrKmap.join('');
     document.getElementById("dimId").innerHTML = dimFilledArrKmap.join('');
+
+    document.getElementById("sopBtn").innerHTML = sopFilledArrKmap.join('');
+    document.getElementById("posBtn").innerHTML = posFilledArrKmap.join('');
+    document.getElementById("fcnEditBtn").innerHTML = fcnEditFilledArrKmap.join('');
+    document.getElementById("kmapBtn").innerHTML = kmapFilledArrKmap.join('');
+    document.getElementById("ttableBtn").innerHTML = ttableFilledArrKmap.join('');
     ch++;
     setTimeout(typeWriterKmap, speed);
   }
